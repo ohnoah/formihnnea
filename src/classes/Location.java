6be53af89f1e456a197da0259b9022c5c473667b
Jack Parkinson <jackparkinson792@gmail.com>
2019-05-15 15:59:50 +0100

    Trying to commit

--- src/classes/Location.java
+++ src/classes/Location.java
@@ -1,30 +1,23 @@
 package classes;
 
-
 import classes.coords.Coordinate;
-import com.google.gson.annotations.SerializedName;
 
 public class Location {
 
     public String name;
-
-    public String id;
-
+    public String googleId;
+    public String owmId;
     public Coordinate position;
 
+    public static Location fromName(String name){ return new Location(name);}
 
+    public Location(String name, String googleId){
+        this.name = name;
+        this.googleId = googleId;
+    }
 
-    public static Location fromId(String id){ return new Location(id, TypeLocation.ID);}
-
-    public static Location fromName(String name){ return new Location(name, TypeLocation.NAME);}
-
-    private Location(String value, TypeLocation mode){
-        if (mode == TypeLocation.ID){
-            this.id = value;
-        }
-        if (mode == TypeLocation.NAME){
-            this.name = value;
-        }
+    public Location(String name){
+        this.name = name;
     }
 
     public Location(){};
