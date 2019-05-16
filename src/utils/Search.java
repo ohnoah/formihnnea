ca3e72ebfaf9d8620dd79cb01c56960ac6e0a1f8
Jack Parkinson <jackparkinson792@gmail.com>
2019-05-16 01:06:27 +0100

    Trying to commit

---
+++ src/utils/Search.java
@@ -0,0 +1,47 @@
+package utils;
+
+import classes.Location;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import org.json.simple.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static utils.QueryParser.callQuery;
+
+public class Search {
+
+    private static final String api_id_google = "AIzaSyAhxpVJs2OH7oYj_LM8DR5Yyzdyl8eMGu0";
+
+
+    public static List<Location> autoCompleteInput(String stringLocation){
+        String query = "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=" + stringLocation + "&key=" + api_id_google;
+        JSONObject jobj = callQuery(query);
+
+        Gson g = new Gson();
+        //System.out.println(jobj);
+
+        JsonObject jsonObject = g.fromJson(jobj.toJSONString(), JsonObject.class);
+
+        List<Location> autoCompResults = new ArrayList<>();
+
+        for (JsonElement job: jsonObject.get("predictions").getAsJsonArray()){
+            autoCompResults.add(new Location(
+                    job.getAsJsonObject().get("description").getAsString(),
+                    job.getAsJsonObject().get("place_id").getAsString()
+            ));
+        }
+
+        return autoCompResults;
+    }
+
+    //Cannot get actual 'current' location - have to ask on startup
+
+
+
+    public static void main(String[] args) {
+        System.out.println(Search.autoCompleteInput("uni").get(0).name);
+    }
+}
