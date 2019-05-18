140a0bf53539902e22eaf85c20d7e0e40ce7f417
Noah O <c9shroud@gmail.com>
2019-05-18 17:57:58 +0100

    Merge branch 'master' of https://github.com/ohnoah/formihnnea
    # Conflicts:
    #	.idea/misc.xml
    
    Half-working version with the detailedpage and the hourly page implemented. Added id's to the controllers.

--- src/sample/DetailsController.java
+++ src/sample/DetailsController.java
@@ -2,12 +2,20 @@ package sample;
 
 import classes.Location;
 import classes.currentweather.CurrentWeather;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.ToggleButton;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.MathContext;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import utils.OWM;
 
 import static sample.Main.*;
@@ -16,35 +24,192 @@ import static sample.Main.*;
 
 public class DetailsController {
 
-    public Label temperature;
-    public ToggleButton btnHourly;
-    public ToggleButton btnWeekly;
-    private Parent root;
-    public Label locate;
-    public Label visibility;
-    public Label chanceOfRain;
-    public Label sunPosition;
-    public Label cloudCover;
-    public Label sunrise;
-    public Label sunset;
 
-    public void displayHourly(){
-        System.out.println("display hourly");
-        System.out.println("country"+ Main.getCountry());
-        System.out.println("city" + Main.getCity());
-    }
+    //set up IDs for all variables in the system
+    @FXML
+    private Label sunPos7;
+
+    @FXML
+    private Label sunPos6;
+
+    @FXML
+    private Label sunPos5;
+
+    @FXML
+    private Label sunPos4;
+
+    @FXML
+    private Label sunPos3;
+
+    @FXML
+    private Label chanceOfRain;
+
+    @FXML
+    private Label sunPos2;
+
+    @FXML
+    private Label sunPos1;
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private Label sunPos8;
+
+    @FXML
+    private Label vis7;
+
+    @FXML
+    private Label vis6;
+
+    @FXML
+    private ImageView btnBack;
+
+    @FXML
+    private ToggleButton btnWeekly;
+
+    @FXML
+    private Label vis5;
+
+    @FXML
+    private Label vis4;
+
+    @FXML
+    private Label vis3;
+
+    @FXML
+    private Label visibility;
+
+    @FXML
+    private Label vis2;
+
+    @FXML
+    private Label vis1;
+
+    @FXML
+    private Label cloudCover;
+
+    @FXML
+    private Label vis8;
+
+    @FXML
+    private Label sunPosition;
+
+    @FXML
+    private ToggleButton btnHourly;
+
+    @FXML
+    private Label sunrise;
+
+    @FXML
+    private Label locate;
+
+    @FXML
+    private Label temp2;
+
+    @FXML
+    private Label temp3;
+
+    @FXML
+    private Label temp1;
+
+    @FXML
+    private Label rain8;
+
+    @FXML
+    private Label cld7;
+
+    @FXML
+    private Label cld8;
+
+    @FXML
+    private Label rain4;
+
+    @FXML
+    private Label cld5;
+
+    @FXML
+    private Label rain5;
+
+    @FXML
+    private Label cld6;
+
+    @FXML
+    private Label rain6;
+
+    @FXML
+    private Label cld3;
+
+    @FXML
+    private Label rain7;
+
+    @FXML
+    private Label cld4;
+
+    @FXML
+    private Label cld1;
+
+    @FXML
+    private Label temperature;
+
+    @FXML
+    private Label cld2;
+
+    @FXML
+    private GridPane hourlyGrid;
+
+    @FXML
+    private Label rain1;
+
+    @FXML
+    private Label rain2;
+
+    @FXML
+    private Label rain3;
+
+    @FXML
+    private ImageView sunType;
+
+    @FXML
+    private Label temp6;
+
+    @FXML
+    private Label temp7;
+
+    @FXML
+    private ScrollPane horizScrollPane;
+
+    @FXML
+    private Label temp4;
+
+    @FXML
+    private Label temp5;
+
+    @FXML
+    private Label temp8;
+
+    @FXML
+    private Label sunset;
+
+    //this is the only parent
+    private Parent root;
+
+
 
     public void back() throws IOException {
-        root = FXMLLoader.load(getClass().getResource("detailpage.fxml"));
+        root = FXMLLoader.load(getClass().getResource("main.fxml"));
         Main.stage.setTitle("Hello World");
         Main.stage.setScene(new Scene(root, 479, 673));
         stage.show();
     }
 
+    //Mihnea's method which initializes all fields in the top part of the screen
     public void initialize(){
         CurrentWeather cw = OWM.getCurrentWeather(new Location(getCity()+","+getCountry()));
         locate.setText(getCity() + ", " + getCountry() );
-        temperature.setText(String.valueOf(cw.mainParameters.temperature - 273.15));
+        BigDecimal bd = new BigDecimal(cw.mainParameters.temperature - 273.15);
+        bd = bd.round(new MathContext(3));
+        temperature.setText(String.valueOf(bd) + "°");
         visibility.setText(cw.visibility);
         sunrise.setText(cw.systemParameters.sunrise.toString().split(" ")[3].substring(0,5));
         sunset.setText(cw.systemParameters.sunset.toString().split(" ")[3].substring(0,5));
